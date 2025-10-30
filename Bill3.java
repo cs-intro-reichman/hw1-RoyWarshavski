7810a5623b62c801912a7f6387fcbd5ce0fffae1
Roy Warshavski <roywarshavski@Roys-MacBook-Pro.local>
2025-10-30 12:33:50 +0200

    exersice1

--- Bill3.java
+++ Bill3.java
@@ -6,9 +6,9 @@ public class Bill3 {
 	public static void main(String[] args) {
 
 		// args of string
-	    String name1 = args[0];
-		String name2 = args[1]; 
-		String name3 = args[2];
+	    String name1 =args[0];
+		String name2 = rgs[1]; 
+		String name3 =args[2];
 		int total = Integer.parseInt(args[3]); // convert string to int 
 
 		double SumPerson = (double) total/3; //convert int to double
